1418f20ea140dd9740930337e77c8ad56120c903
Aftershamon <111443736+Aftershamon@users.noreply.github.com>
2024-06-15 23:04:29 +0700

    Update page.tsx

--- src/app/[locale]/(auth)/dashboard/page.tsx
+++ src/app/[locale]/(auth)/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { getTranslations } from 'next-intl/server';
+
 export async function generateMetadata(props: { params: { locale: string } }) {
   const t = await getTranslations({
     locale: props.params.locale,
