6cdef2b8ff52f8530dc389c9337eca15f378234d
Aftershamon <111443736+Aftershamon@users.noreply.github.com>
2024-06-15 22:09:18 +0700

    Update page.tsx

--- src/app/[locale]/(unauth)/page.tsx
+++ src/app/[locale]/(unauth)/page.tsx
@@ -1,7 +1,7 @@
 import { getTranslations } from 'next-intl/server';
 
 
-import CardItem from "@/components/CardItem";
+import CardItem from '@/components/CardItem';
 export async function generateMetadata(props: { params: { locale: string } }) {
   const t = await getTranslations({
     locale: props.params.locale,
@@ -17,8 +17,8 @@ export async function generateMetadata(props: { params: { locale: string } }) {
 export default function Index() {
   return (
     <>
-      <h5 className="text-3xl font-bold text-zinc-50 mt-5">Highlight</h5>
-      <div className="grid grid-cols-4 gap-1">
+      <h5 className='text-3xl font-bold text-zinc-50 mt-5'>Highlight</h5>
+      <div className='grid grid-cols-4 gap-1'>
         <CardItem />
         <CardItem />
         <CardItem />
