4c07fd954df23a4932b5ac3f5320ac517f37378f
Aftershamon <111443736+Aftershamon@users.noreply.github.com>
2024-06-15 21:28:49 +0700

    Create CardItem.tsx

---
+++ src/components/CardItem.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+export default function ActionAreaCard() {
+  return (
+    <Card
+      sx={{
+        maxWidth: 500,
+        maxHeight: 500,
+        minHeight: "auto",
+        marginTop: "20px",
+        borderRadius: 5,
+      }}
+    >
+        <CardMedia
+          component="img"
+          sx={{ height: 400 }}
+          image="https://f.ptcdn.info/379/084/000/lwznke79ikK9wumEfFU-o.png"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="div">
+            [Pantip Point] เตียงนอนไม่ต้อง! เตรียมนอนดึกดูยูโร 2024!!
+          </Typography>
+        </CardContent>
+    </Card>
+  );
+}
