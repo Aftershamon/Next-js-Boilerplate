81fe46edbc7fe197c33038690516bad34425fa79
Aftershamon <111443736+Aftershamon@users.noreply.github.com>
2024-06-15 22:08:20 +0700

    Update CardItem.tsx

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
 export default function ActionAreaCard() {
   return (
@@ -10,19 +10,19 @@ export default function ActionAreaCard() {
       sx={{
         maxWidth: 500,
         maxHeight: 500,
-        minHeight: "auto",
-        marginTop: "20px",
+        minHeight: 'auto',
+        marginTop: '20px',
         borderRadius: 5,
       }}
     >
         <CardMedia
-          component="img"
+          component='img'
           sx={{ height: 400 }}
-          image="https://f.ptcdn.info/379/084/000/lwznke79ikK9wumEfFU-o.png"
-          alt="green iguana"
+          image='https://f.ptcdn.info/379/084/000/lwznke79ikK9wumEfFU-o.png'
+          alt='green iguana'
         />
         <CardContent>
-          <Typography gutterBottom variant="h6" component="div">
+          <Typography gutterBottom variant='h6' component='div'>
             [Pantip Point] เตียงนอนไม่ต้อง! เตรียมนอนดึกดูยูโร 2024!!
           </Typography>
         </CardContent>
